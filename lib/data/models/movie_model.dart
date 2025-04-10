8b203051415a1740db32e8037a6d811fef9eb4db
Marli Mota <marli.mota42@gmail.com>
2025-04-10 17:03:20 -0300

    feat: add movie model to data layer

---
+++ lib/data/models/movie_model.dart
@@ -0,0 +1,25 @@
+import 'package:pipoca_app/domain/entities/movie_entity.dart';
+
+class MovieModel extends MovieEntity {
+  const MovieModel({
+    required super.title,
+    required super.posterPath,
+    required super.backdropPath,
+    required super.overview,
+    required super.releaseDate,
+    required super.voteAverage,
+    required super.id,
+  });
+
+  factory MovieModel.fromJson(Map<String, dynamic> json) {
+    return MovieModel(
+      title: json['title'],
+      posterPath: json['poster_path'],
+      backdropPath: json['backdrop_path'],
+      overview: json['overview'],
+      releaseDate: json['release_date'],
+      voteAverage: json['vote_average'],
+      id: json['id'],
+    );
+  }
+}
