b298af16cc8e20cf4c0be1e324c68623753aa844
Marli Mota <marli.mota42@gmail.com>
2025-04-10 16:39:23 -0300

    feat: add get movies usecase to domain layer

---
+++ lib/domain/usecases/get_movie_usecase.dart
@@ -0,0 +1,18 @@
+import 'package:dartz/dartz.dart';
+import 'package:pipoca_app/core/erros/failure.dart';
+import 'package:pipoca_app/domain/entities/movie_entity.dart';
+import 'package:pipoca_app/domain/repositories/movie_repository.dart';
+
+class GetMovieUsecase {
+  final MovieRepository repository;
+
+  GetMovieUsecase(this.repository);
+
+  Future<Either<Failure, List<MovieEntity>>> getTopRatedMovies() async {
+    return await repository.getTopRatedMovies();
+  }
+
+  Future<Either<Failure, List<MovieEntity>>> getPopularMovies() async {
+    return await repository.getPopularMovies();
+  }
+}
